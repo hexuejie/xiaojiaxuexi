84ca1ffc5fec390d90bf4f611854a68bb173f7b4
hexuejie <673219930@qq.com>
2019-03-21 10:55:05 +0800

    项目移植

---
+++ hellolua2/proj.ios_mac/LearnFM.framework/Headers/LearnFM.h
@@ -0,0 +1,20 @@
+//
+//  LearnFM.h
+//  LearnFM
+//
+//  Created by 何学杰 on 2018/10/23.
+//  Copyright © 2018年 何学杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LearnFM.
+FOUNDATION_EXPORT double LearnFMVersionNumber;
+
+//! Project version string for LearnFM.
+FOUNDATION_EXPORT const unsigned char LearnFMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LearnFM/PublicHeader.h>
+
+#import <LearnFM/GLMusicPlayViewController.h>
+#import <LearnFM/ESNavigationController.h>
